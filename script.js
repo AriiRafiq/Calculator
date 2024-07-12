9009d59020f3f2c998f606798ccd08e5fac44bb4
Ari <ari.rafiq.ph@gmail.com>
2024-07-12 15:19:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+function calculateResult() {
+    var num1 = parseFloat(document.getElementById('number1').value) || 0;
+    var num2 = parseFloat(document.getElementById('number2').value) || 0;
+    var num3 = parseFloat(document.getElementById('number3').value) || 0;
+    var num4 = parseFloat(document.getElementById('number4').value) || 0;
+    var num5 = parseFloat(document.getElementById('number5').value) || 0;
+    var num6 = parseFloat(document.getElementById('number6').value) || 0;
+    var num7 = parseFloat(document.getElementById('number7').value) || 0;
+    var num8 = parseFloat(document.getElementById('number8').value) || 0;
+    var num9 = parseFloat(document.getElementById('number9').value) || 0;
+    var num10 = parseFloat(document.getElementById('number10').value) || 0;
+
+    var A = num1 - num2;
+    var B = num3;
+    var C = num5 - num4;
+    var D = num6;
+    var E = num8 - num7;
+    var F = num10 - num9;
+
+    var result = D + F + A - B - C - E;
+
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var inputs = document.querySelectorAll('input[type="number"]');
+    inputs.forEach((input, index) => {
+        input.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                } else {
+                    calculateResult();
+                }
+            }
+        });
+    });
+});
